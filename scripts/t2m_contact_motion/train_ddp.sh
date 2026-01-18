d88152265b2f583df5e0193202de9829c06ed34f
syr-macmin <syrelink@gmail.com>
2026-01-18 21:11:07 +0800

    11

--- scripts/t2m_contact_motion/train_ddp.sh
+++ scripts/t2m_contact_motion/train_ddp.sh
@@ -6,7 +6,7 @@ then
     PORT=29500
 fi
 
-CUDA_VISIBLE_DEVICES=0,1 torchrun --nnodes=1 --nproc_per_node=2 --rdzv_backend=c10d --rdzv_endpoint=localhost:${PORT} train_ddp.py \
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.run --nnodes=1 --nproc_per_node=2 --rdzv_backend=c10d --rdzv_endpoint=localhost:${PORT} train_ddp.py \
             hydra/job_logging=none hydra/hydra_logging=none \
             exp_name=${EXP_NAME} \
             output_dir=outputs \
