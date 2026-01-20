9b5945ad84372f17d3c1d993049e2f9f062682ab
shiboliu <860745337@qq.com>
2026-01-20 13:20:05 +0800

    11

--- scripts/t2m_contact_motion/train_ddp.sh
+++ scripts/t2m_contact_motion/train_ddp.sh
@@ -18,7 +18,9 @@ CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.run --nnodes=1 --nproc_per_
             task.train.max_steps=600000 \
             task.train.save_every_step=25000 \
             task.dataset.train_transforms=['RandomEraseLang','RandomEraseContact','NumpyToTensor'] \
+            +task.dataset.pred_contact_dir=map_pointmamba \
+            task.dataset.mix_train_ratio=0.5 \
             model=cmdm \
-            model.arch='dit' \
+            model.arch='trans_enc' \
             model.data_repr='h3d' \
             model.text_model.max_length=20
